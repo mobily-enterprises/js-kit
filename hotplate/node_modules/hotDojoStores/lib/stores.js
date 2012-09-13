956b2e230fa77be6010936f449214b87b0bef113
Tony Mobily <merc@mobily1.com>
2012-09-13 15:14:04 +0800

    Initial commit for dojo store

---
+++ hotplate/node_modules/hotDojoStores/lib/stores.js
@@ -0,0 +1,83 @@
+define([
+  'app/JsonRest',
+  'dojo/domReady!',
+  ],function(
+  JsonRest
+  ){
+
+
+  // Stores
+  var stores = {},
+      path,
+      isAnon, isUser;
+  
+
+  // Initialise tokenValue (might not be set)
+  if( typeof(workspaceIdValue) == 'undefined' ){
+    workspaceIdValue = null;
+  }
+ 
+
+  // Make all of the stores, which will be used across the application
+  [
+    // Non-data calls
+    'recoverAnon',
+    'loginAnon',
+    'logoutUser',
+
+    // Data calls
+    'workspacesAnon',
+    'usersAnon', 
+    'workspacesUser',
+
+    // API calls (need a token ID to work)
+    'users',
+    'roles',
+  ].forEach( function(i){
+
+    // Check if the call is anonymous
+    isAnon = i.match(/Anon$/);
+    isUser = i.match(/User$/);
+
+    // tokenValue = '873535f1b3c3cfffe76a68dfc9048647';
+     
+    // Either the token must be set, or the call needs to be a /user or /anon call
+    if( workspaceIdValue || isAnon || isUser ){
+
+      // Set the path depending on the store's name, to keep URL namespace clean
+			// path = isAnon ? '/anon/' : ( isUser ?  '/user/' : '/api/1/' + tokenValue + '/') ;
+      path = isAnon ? '/anon/' : ( isUser ?  '/user/' : ('/call/' + workspaceIdValue + '/') ) ;
+
+      console.log("Creating store " + path + i + '/');
+
+      // Create the store
+      stores[i] = new JsonRest({
+        target: path + i + '/',
+		  	idProperty: '_id',
+        sortParam: 'sortBy',
+      });
+    }
+  });
+
+  return stores;
+});
+
+
+    /*
+    // Quick function to cancel a submit button IF present
+    // This will need improving: it basically tries to call "cancel" of the first button in the
+    // first form it finds
+    function cancelSubmitButton(){
+
+      // Find the submit button within the form. A bit of guesswork here,
+      // will need to improve for forms with multiple buttons
+      var submitButton = registry.byId( query("form#"+form.id+" span[role='button']")[0].id );
+
+      // If a .cancel() method is defined, call it and make the button unbusy
+      if( typeof(submitButton) == 'object' && submitButton.cancel){
+       submitButton.cancel();
+      }
+    }
+    */
+
+
