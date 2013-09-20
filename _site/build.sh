8c036e09490419f9114c2b8748cf42deb542fba7
Tony Mobily <tonymobily@gmail.com>
2013-09-20 15:18:36 +0800

    Automatic build done

--- _site/build.sh
+++ _site/build.sh
@@ -6,16 +6,14 @@ cat _includes/top_page.txt hotplate/GUIDE.md  > guide.md
 echo Copying README.md onto index.md...
 cat _includes/top_page.txt hotplate/README.md  > index.md
 
-echo Building API
-
-echo Building side with Jekyll...
-jekyll build
-
 echo Building server API...
 yuidoc -t _yuidocTheme  -o serverAPI hotplate/node_modules/*/lib/
 
 echo Building client API...
 yuidoc -t _yuidocTheme  -o clientAPI hotplate/node_modules/*/client/
+
+echo Building side with Jekyll...
+jekyll build
 
 echo Submitting site to GitHut pages...
 git add *;
