266474170dd33734543d438506402136dfbe90c4
Tony Mobily <tonymobily@gmail.com>
2015-06-05 22:20:05 +0800

    Cosmetic changes

--- hotplate.js
+++ hotplate.js
@@ -7,7 +7,7 @@ var async = require('async')
 var hotplate = exports;
 
 // Hotplate's core functionality
-hotplate.require = function( m ){ return require( m ); }
+hotplate.require = function( m ){ return require( m ); };
 hotplate.cachable = async.memoize;
 hotplate.hotEvents = new EventEmitterCollector();
 hotplate.config = new DeepObject();
@@ -18,11 +18,12 @@ var origEmitCollect = hotplate.hotEvents.emitCollect;
 hotplate.hotEvents.emitCollectCollect = function(){
   debug("Emitted event: %o", arguments );
   origEmitCollect.apply( this, arguments );
-}
+};
 // Hotplate's sane defaults
 // You can (and should) over-ride them in your server.js file
 hotplate.config.set( 'hotplate.moduleFilesPrefix', '/hotplate' ); // hotClientXXX modules will use it as base URL
 hotplate.config.set( 'hotplate.routeUrlsPrefix', '/pages' ); // hotCoreAuth uses it as base URL
+
 // Db settings
 hotplate.config.set( 'hotplate.db', null );
 hotplate.config.set( 'hotplate.DbLayerMixin', function(){ } );
