ec361e2624647fab15a2a60ea45d02ec80cf6dd4
merc <merc@mobily1.com>
2017-12-05 12:30:36 +0800

    Fixed adding of own tabId

--- core_modules/hotCoreComet/lib/hotCoreComet.js
+++ core_modules/hotCoreComet/lib/hotCoreComet.js
@@ -211,7 +211,7 @@ var conditionalTabDispatch = exports.conditionalTabDispatch = function( ce, sele
           consolelog("Selector passed, dispatching a message!")
           makeMessage( ce, tab, tabSession, ce.message, function( err, newMessage){
             consolelog("Will dispatch: ", newMessage )
-            message.fromTabId = tab.id
+            newMessage.fromTabId = ce.fromTabId
             r.push({ to: tab.id, message: newMessage });
             cb( null );
           });
