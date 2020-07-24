59b638351e1947311164a6c1ea5b0e1598c8ea38
Tony Mobily <tonymobily@gmail.com>
2020-07-24 09:25:37 +0800

    WIP

--- bin/kit.js
+++ bin/kit.js
@@ -1,34 +1,34 @@
 #!/usr/bin/env node
-const execSync = require('child_process').execSync
+// const execSync = require('child_process').execSync
 const fs = require('fs')
+const copy = require('recursive-copy')
 
 run(...process.argv)
 
 async function run (node, cmd, op, p1, p2, p3) {
-  console.error(`op: ${op}, p1: ${p1}`)
   switch (op) {
     case 'add':
       installKit(p1)
       break
-
-    case 'testRun':
-      console.error('testRun was here')
-      break
-
-    case 'cleanup':
-      execSync(`rm ../../../${p1}`)
-      break
   }
 }
 
-function installKit (kitName) {
+async function installKit (kitName) {
+  const kitDir = `${__dirname}/../kits/${kitName}`
+  const kitInstallFile = `${__dirname}/../installed/${kitName}`
+
+  if (!isDir(kitDir)) {
+    console.log(`FATAL: Kit not found: ${kitName}`)
+    process.exit(1)
+  }
+
   console.log('Installing:', kitName)
-  if (fs.existsSync(`${__dirname}/../installed/${kitName}`)) {
+  if (fs.existsSync(kitInstallFile)) {
     console.log(`${kitName} already installed, skipping...`)
     return
   }
 
-  const kjson = require(`${__dirname}/../kits/${kitName}/kit.json`)
+  const kjson = require(`${kitDir}/kit.json`)
   const deps = kjson.kitDependencies || []
   if (deps.length) {
     console.log('This module has dependencies. Installing them.', deps)
@@ -39,13 +39,32 @@ function installKit (kitName) {
 
   // Install this kit
 
-  // Check that kit is not already installed (check lock file)
+  if (isDir(`${kitDir}/distr`)) {
+    console.log('distr folder found, copying files over')
+    console.log(process.env, __dirname)
+    // await copy(`${kitDir}/distr`,
+
+  }
+
   // Install it
   //   copy files in distr
   //   add npmDependencies to package.json (if not there already)
   //   load js-kit.inserts, make inserts
 
   // Mark it as installed in metadata (create lock file)
-  fs.writeFileSync(`${__dirname}/../installed/${kitName}`, kjson.version)
+  fs.writeFileSync(kitInstallFile, kjson.version)
   console.log('kit installed:', kitName)
+}
+
+function isDir (path) {
+  try {
+    // Query the entry
+    const stats = fs.lstatSync(path)
+    if (stats.isDirectory()) return true
+    return false
+  }
+  catch (e) {
+    if (e.code === 'ENOENT') return false
+    throw e
+  }
 }
