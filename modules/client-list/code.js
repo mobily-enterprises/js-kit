770fa15718b71988b8cbe654c2f657187cbb968a
Tony Mobily <tonymobily@gmail.com>
2020-12-21 12:02:37 +0800

    Improvements

---
+++ modules/client-list/code.js
@@ -0,0 +1,18 @@
+
+exports.getPromptsHeading = (config) => {
+}
+
+exports.getPrompts = (config) => {
+}
+
+exports.prePrompts = (config) => {
+}
+
+exports.preAdd = (config) => {
+}
+
+exports.postAdd = (config) => {
+}
+
+exports.fileRenamer = (config, file) => {
+}
