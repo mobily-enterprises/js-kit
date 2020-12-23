c1d91b53869794e44ba418573750d5bad2b77a89
Tony Mobily <tonymobily@gmail.com>
2020-12-23 08:03:34 +0800

    WIP

--- modules/client-app-frame/code.js
+++ modules/client-app-frame/code.js
@@ -1,14 +1,23 @@
-
 exports.getPromptsHeading = (config) => {
+  return "Pick the previx of your app's elements. If you pick 'my', elements will be 'my-something', 'my-something-else' and so on"
 }
 
 exports.getPrompts = (config) => {
+  return [
+    {
+      type: 'text',
+      name: 'elPrefix',
+      message: 'Element name prefix',
+      initial: 'my'
+    }
+  ]
 }
 
 exports.prePrompts = (config) => {
 }
 
 exports.preAdd = (config) => {
+  config.vars.elPrefix =  config.userInput['client-app-frame'].elPrefix
 }
 
 exports.postAdd = (config) => {
