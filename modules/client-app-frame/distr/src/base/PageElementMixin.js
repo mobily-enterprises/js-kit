3dd41ea80acc3540ec8ac4aec12af295bfc8e1b0
Tony Mobily <tonymobily@gmail.com>
2020-12-31 11:32:36 +0800

    Titles set properly

--- modules/client-app-frame/distr/src/base/PageElementMixin.js
+++ modules/client-app-frame/distr/src/base/PageElementMixin.js
@@ -2,6 +2,7 @@ import { html, css } from 'lit-element'
 import { ifDefined } from 'lit-html/directives/if-defined'
 import { RoutingPageMixin } from 'routify/lit/RoutingPageMixin'
 import '../ui-elements/<%=vars.elPrefix%>-page-header'
+import { updateMetadata } from 'pwa-helpers/metadata.js'
 
 export const PageElementMixin = (base) => {
   return class Base extends RoutingPageMixin(base) {
@@ -27,7 +28,12 @@ export const PageElementMixin = (base) => {
       this.autoload = true
     }
 
-    routerCallback () {}
+    routerCallback () {
+      updateMetadata({
+        title: '<%=userInput['client-app-frame'].appName%>' + ' - ' + this.pageTitle,
+        description: this.pageTitle
+      })
+    }
 
     shouldUpdate (changed) {
       // If a  page goes from inactive to active, dispatch a very useful "activated" event
