65e6d19bd750f03c4ed8631acd57959e79afd8fb
Tony Mobily <tonymobily@gmail.com>
2021-01-04 19:22:54 +0800

    WIP

--- modules/client-app-page-tabs/code.js
+++ modules/client-app-page-tabs/code.js
@@ -23,7 +23,7 @@ exports.boot = (config) => {
 exports.preAdd = (config) => {
 }
 
-exports.postAdd = (config) => {
+exports.postAdd = async (config) => {
   config.utils.prompts( {
     type: 'text',
     name: 'stocazzo',
@@ -33,6 +33,25 @@ exports.postAdd = (config) => {
   )
   console.log("TEST 1:", config.utils.walk(config.dstDir))
   console.log("TEST 2:", config.utils.findAnchorPoints('<!-- Element insertion point -->', config.dstDir))
+
+
+
+  const manipulations = {
+    text: {
+      "":[
+        {
+           "op":"insert",
+           "position":"after",
+           "newlineAfter":true,
+           "anchorPoint":"<!-- Element insertion point -->",
+           "valueFromFile":"element-tabs.js"
+        }
+      ]
+    }
+  }
+  await config.utils.executeManipulations(manipulations, )
+
+
 }
 
 exports.fileRenamer = (config, file) => {
