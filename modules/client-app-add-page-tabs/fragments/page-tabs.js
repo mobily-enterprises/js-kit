60453d34a0edf05eb07e03960f4c7bcb76ce998f
Tony Mobily <tonymobily@gmail.com>
2021-01-07 16:49:07 +0800

    WIP

--- modules/client-app-add-page-tabs/fragments/page-tabs.js
+++ modules/client-app-add-page-tabs/fragments/page-tabs.js
@@ -1,5 +1,5 @@
 <ee-tabs use-hash passive id="<%=userInput['client-app-add-page-tabs'].elementId%>" default="" name-attribute="tab-name">
-  <!-- Element tab heading insertion point -->
+  <!-- Page tab heading insertion point -->
 
-  <!-- Element tab tab insertion point -->
+  <!-- Page tab tab insertion point -->
 </ee-tabs>
