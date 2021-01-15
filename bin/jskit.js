58e72f6586bf1a8af16ef79b93160bbf1c769509
Tony Mobily <tonymobily@gmail.com>
2021-01-15 08:46:35 +0800

    Parameters normalised

--- bin/jskit.js
+++ bin/jskit.js
@@ -1,15 +1,7 @@
 #!/usr/bin/env node
-
-debugger
 const { program } = require('../node_modules/commander')
-const add = require('../node_modules/scaffoldizer/commands/add')
-
 const jskitPackageJson = require('../package.json')
-const commands = {
-  add: modules => add(`${__dirname}/..`, process.cwd(), modules)
-  }
-
-// const globals = require('./globals')
+const commands = require('../node_modules/scaffoldizer/commands')
 
 const increaseVerbosity = (dummyValue, previous) => previous++
 
@@ -22,12 +14,14 @@ async function main () {
 
   // The main command: add
   program.option('-v, --verbose', 'verbosity that can be increased', increaseVerbosity, 0)
-
-  await program.parseAsync(process.argv)
+  //
+  //
 
   program.command('add [modules...]')
-    .description('Add modules to project in current directory. You can optionally pass a list of modules to install. Otherwise, a UI will be initiated to pick which modules should be installed')
-    .action(commands.add)
+    .description('Add js-kit modules to the project in the current directory. If no module is specified, a user interface will launch.')
+    .action(modules => {
+      commands.add(`${__dirname}/..`, process.cwd(), modules)
+    })
 
   await program.parseAsync(process.argv)
 }
