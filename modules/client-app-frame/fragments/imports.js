b8afce6567d1e3e31cea2f6ab483db1f4507988c
freudFlintstone <raphael.mattos@contextaware.com.br>
2021-09-03 20:28:18 -0300

    preparing for tpe2

--- modules/client-app-frame/fragments/imports.js
+++ modules/client-app-frame/fragments/imports.js
@@ -1,4 +1,14 @@
 // Imports for client-app-frame
+
+// New TPE import
+// import { myTheme } from 'my-tpe-theme.js'
+// import { tpe } from '../tpe-class.js'
+// tpe.importTheme(myTheme)
+// await tpe.importer()
+// Default import with TPE2
+// import 'tpe2/tpe.js'
+
+// Legacy TPE import 
 import 'tpe/tpe.js'
 import { MainPageMixin } from 'routify/lit/MainPageMixin'
 import { activateElement } from 'routify/routify'
