519a2a88fd8c85e84ff86fd895caf75d1ad4fd4f
Tony Mobily <tonymobily@gmail.com>
2023-06-13 14:07:34 +0800

    WIP

--- modules/client-app-stores/distr/src/lib/stores/AddElement.js
+++ modules/client-app-stores/distr/src/lib/stores/AddElement.js
@@ -1,7 +1,7 @@
 /* Loaded modules -- start */
 import AppElement from '../base/AppElement.js'
 import { RoutingMixin } from '../base/AppElementMixins/RoutingMixin.js'
-import { AddEditStoreMixin } from '../stores/AddEditStoreMixin'
+import { AddEditStoreMixin } from './mixins/AddEditStoreMixin'
 /* Loaded modules -- end */
 
 export class AddElement extends RoutingMixin(AddEditStoreMixin(AppElement)) {
